5c84187fc06b3fa4b6d0c8215e7a2c073de16265
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2025-10-30 19:48:48 +0100

    Refactor commit types & words

--- src/metrics/commits/types.clj
+++ src/metrics/commits/types.clj
@@ -14,6 +14,7 @@
    :test
    :perfs
    :doc
+   :types
    :tools
    :ci
    :version
@@ -29,6 +30,7 @@
         :weak-refacto "#ef9fff"
         :test "#176796"
         :doc "#67beff"
+        :types "#97beff"
         :perfs "#17becf"
         :tools "#FFFF00"
         :ci "#CCCC00"
@@ -40,26 +42,27 @@
   (let [description (clojure.string/lower-case description)]
     (cond
       ; keywords
-      (re-find #"(?:^|[^\w])(bug|fix|revert|typo|error)" description) :fix
-      (re-find #"(?:^|[^\w])refacto" description) :refacto
-      (re-find #"(?:^|[^\w])(feat)" description) :feat
-      (re-find #"(?:^|[^\w])test" description) :test
-      (re-find #"(?:^|[^\w])perf" description) :perfs
-      (re-find #"(?:^|[^\w])ci" description) :ci
-      (re-find #"(?:^|[^\w])docs?" description) :doc
-      (re-find #"(?:^|[^\w])(chore|build|lint)" description) :tools
-      (re-find #"(?:^|[^\w])version" description) :version
+      (re-find #"(?:^|\W)(bug|fix|revert|typo|error)" description) :fix
+      (re-find #"(?:^|\W)(refacto|review|tech|requested change|recycle)" description) :refacto
+      (re-find #"(?:^|\W)(feat)" description) :feat
+      (re-find #"(?:^|\W)test" description) :test
+      (re-find #"(?:^|\W)perf" description) :perfs
+      (re-find #"(?:^|\W)ci" description) :ci
+      (re-find #"(?:^|\W)docs?" description) :doc
+      (re-find #"(?:^|\W)types?" description) :types
+      (re-find #"(?:^|\W)(chore|build|lint)" description) :tools
+      (re-find #"(?:^|\W)version" description) :version
       ; words combinations
-      (and (re-find #"(?:^|[^\w])add" description)
-           (re-find #"(?:^|[^\w])remove" description)) :refacto
+      (and (re-find #"(?:^|\W)add" description)
+           (re-find #"(?:^|\W)remove" description)) :refacto
       ; unique words - weak
-      (re-find #"(?:^|[^\w])(avoid|correc|guard|workaround)" description) :weak-fix
-      (re-find #"(?:^|[^\w])(add|enhance|implement)" description) :feat
-      (re-find #"(?:^|[^\w])(cli|make|parse|style|use)" description) :weak-feat
-      (re-find #"(?:^|[^\w])(allow|clean|delete|drop|improve|move|remove|rename|restruct|rework|simplify|swap|update)" description) :weak-refacto
-      (re-find #"(?:^|[^\w])(backport|bump|upgrade|lock|dependenc|merge)" description) :weak-tools
+      (re-find #"(?:^|\W)(avoid|correc|guard|workaround|check|🐛)" description) :weak-fix
+      (re-find #"(?:^|\W)(add|enhance|implement)" description) :feat
+      (re-find #"(?:^|\W)(cli|implem|make|new|parse|sparkles|✨|style|use|wip)" description) :weak-feat
+      (re-find #"(?:^|\W)(allow|clean|delete|drop|improve|move|remove|rename?|restruct|rework|simplify|swap|update)" description) :weak-refacto
+      (re-find #"(?:^|\W)(backport|bump|upgrade|lock|dependenc|merge)" description) :weak-tools
       ; pattern
-      (re-find #"(?:^|[^\w])(?:v|ver|version)?\d+\.\d+(?:\.\d+)?(?:[^\w]|$)" description) :version
+      (re-find #"(?:^|\W)(?:v|ver|version)?\d+\.\d+(?:\.\d+)?(?:\W|$)" description) :version
       :else :unknown)))
 
 (defn commits-with-type
